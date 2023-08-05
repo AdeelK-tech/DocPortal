e285f1ac57f12f50bac9b21b0cb86c791c6df38b
AdeelK-tech <adeeltech12@gmail.com>
2023-08-05 14:57:00 +0500

    images size optimization

--- src/transporterInstance.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require("nodemailer");
-var transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "adeel98981@gmail.com",
-    pass: "Adeel123",
-  },
-});
-module.exports(transporter);
